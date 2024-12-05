b67c6edc0f7e0b21a441c4022fe81e62c72d167d
biedroneczka <naprywatne@duck.com>
2024-12-05 08:50:02 +0100

    day5

---
+++ 5/5.py
@@ -0,0 +1,34 @@
+sum1 = 0
+sum2 = 0
+
+with open('input.txt') as input:
+    # part 1
+    lines = input.readlines()
+    count = 0
+    for line in lines:
+        if line == "\n":
+            break
+        count += 1
+    rules = lines[:count]
+    checks = lines[count:]
+    for rule in range(len(rules)):
+        rules[rule] = rules[rule].replace("\n","")
+    for check in range(len(checks)):
+        checks[check] = checks[check].replace("\n","")
+    
+    for line in checks:
+        line = line.split(",")
+        legal = True
+        for number in range(len(line)):
+            for checkEvery in range((number+1),len(line)):
+                if (str(line[checkEvery])+"|"+str(line[number])) in rules:
+                    legal = False
+        if legal:
+            middleIndex = int(((len(line)+1)/2)-1)
+            print(line[middleIndex])
+            try:
+                sum1 += int(line[middleIndex])
+            except:
+                pass #probably some misused "\n" or something
+
+print(sum1,sum2)
