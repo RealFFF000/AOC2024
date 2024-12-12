a6a6eb8040657360ddd70a6fd489ba18c228cf14
biedroneczka <naprywatne@duck.com>
2024-12-12 18:20:30 +0100

    day12Working1

--- 12/12.py
+++ 12/12.py
@@ -1,72 +1,50 @@
 sum1 = 0
-sum2 = 0
-characters = []
-groupAmounts = []
-uniqueGroupsLines = []
-visited = []
 directions = [[-1,0],[0,1],[1,0],[0,-1]]
+toDraw = 1
+blockArea = [0]
+blockPeremiter = [0]
+map = []
+def printMap():
+    for line in map:
+        for character in line:
+            print(character,end="")
+        print("")
 
-def paint(line,character,string):
-    initialString = str(lines[line][character])
-    if uniqueGroupsLines[line][character] == []:
-        uniqueGroupsLines[line][character] = string
-    visited.append([line,character])
+def explore(y,x,characterToDrawWith):
+    blockArea[0]+=1
+    map[y][x] = characterToDrawWith
     for direction in directions:
-        try:
-            if lines[line+direction[0]][character+direction[1]] == initialString and [line+direction[0],character+direction[1]] not in visited:
-                paint(line+direction[0],character+direction[1],string)
-        except:
-            pass
-        
+        if y+direction[0]>=0 and (y+direction[0])<(len(lines)) and x+direction[1]>=0 and (x+direction[1])<(len(lines[y])):
+            #print("comparing",lines[y+direction[0]][x+direction[1]],"with",lines[y][x])
+            if lines[y+direction[0]][x+direction[1]] == lines[y][x] and map[y+direction[0]][x+direction[1]] == 0:
+                explore(y+direction[0],x+direction[1],characterToDrawWith)
+            if not lines[y+direction[0]][x+direction[1]] == lines[y][x]:
+                blockPeremiter[0]+=1
+        else:
+            blockPeremiter[0]+=1
+            #print("apparently",y+direction[0],x+direction[1],"is considrered out of bounds")
+
 with open('input.txt') as input:
-    # part 1
-    # firstly, let's make every individual group of a character unique character
-    # I'm gonna do this by filling every group with (character+group number) scheme
     lines = input.readlines()
     for line in range(len(lines)):
         lines[line] = lines[line].replace("\n","")
-        uniqueGroupsLines.append([])
+        map.append([])
         for character in range(len(lines[line])):
-            uniqueGroupsLines[line].append([])
+            map[line].append(0)
+
     for line in range(len(lines)):
         for character in range(len(lines[line])):
-            if len(lines[line][character]) == 1:
-                if lines[line][character] not in characters and [line,character] not in visited:
-                    characters.append(lines[line][character])
+            if map[line][character] == 0:
+                printMap()
+                print("")
+                blockArea = [0]
+                blockPeremiter = [0]
+                explore(line,character,toDraw)
+                print(lines[line][character])
+                print(blockArea[0],blockPeremiter[0])
+                sum1+=(blockArea[0]*blockPeremiter[0])
+                toDraw += 1
+                if toDraw == 10: toDraw = 1
 
-                    groupAmounts.append(0)
-                else:
-                    if uniqueGroupsLines[line][character] == []:
-                        groupAmounts[characters.index(lines[line][character])]+=1
-                        paint(line,character,str(lines[line][character])+str(groupAmounts[characters.index(lines[line][character])]))
-                    
-                
-                #this block has not been filled yet
-    for line in lines:
-        print(line)
-    for line in uniqueGroupsLines:
-        print(line)
-    print(characters,groupAmounts,sep="\n")
-    
-    charactersIncludingVariety = []
-    amounts = []
-    perimeters = []
-    for line in range(len(uniqueGroupsLines)):
-        for character in range(len(uniqueGroupsLines[line])):
-            if uniqueGroupsLines[line][character] not in charactersIncludingVariety:
-                charactersIncludingVariety.append(uniqueGroupsLines[line][character])
-                amounts.append(1)
-                perimeters.append(0)
-            else:
-                amounts[charactersIncludingVariety.index(uniqueGroupsLines[line][character])] += 1
-            for direction in directions:
-                try:
-                    if uniqueGroupsLines[line+direction[0]][character+direction[1]] != uniqueGroupsLines[line][character]:
-                        perimeters[charactersIncludingVariety.index(uniqueGroupsLines[line][character])]+=1
-                except:
-                    #found edge but dont care
-                    perimeters[charactersIncludingVariety.index(uniqueGroupsLines[line][character])]+=1
-    print(charactersIncludingVariety,amounts,perimeters,sep="\n")
-    for a in range(len(perimeters)):
-        sum1+=(perimeters[a]*amounts[a])
-print(sum1,sum2)
+print(sum1)
+
